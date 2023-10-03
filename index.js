da7c9b50c8668679face5b1149348f6cd6e11836
samira toubal seghir <samsidt@gmail.com>
2023-10-03 19:29:42 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const main = document.getElementsByTagName("main")[0];
+main.remove()
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = "victory"; 
+
+newHeader.textContent = "is the champion"; 
